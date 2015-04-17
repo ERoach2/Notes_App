f4cf02002fde2602dcc29692dcb2cbf1c2a551ca
Edward <emroach2@gmail.com>
2015-04-17 17:02:07 -0400

    Test Commit

--- app/src/main/java/com/mycompany/notespreview/chap.java
+++ app/src/main/java/com/mycompany/notespreview/chap.java
@@ -13,7 +13,7 @@ public class chap extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chap);
     }
-    //Hello
+    //Hello World Show commentsd
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
